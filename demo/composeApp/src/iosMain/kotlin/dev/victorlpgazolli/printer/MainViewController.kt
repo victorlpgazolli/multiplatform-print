1f0722b16c65e2cc9b0c55ee6911d55ecc8d5b3d
victorlpgazolli <victorlpgazolli@gmail.com>
2025-03-09 00:27:50 -0300

    ref: add koin init to ios demo

--- demo/composeApp/src/iosMain/kotlin/dev/victorlpgazolli/printer/MainViewController.kt
+++ demo/composeApp/src/iosMain/kotlin/dev/victorlpgazolli/printer/MainViewController.kt
@@ -1,41 +1,16 @@
 package dev.victorlpgazolli.printer
 
 import androidx.compose.ui.window.ComposeUIViewController
-import platform.Foundation.NSDocumentDirectory
-import platform.Foundation.NSFileManager
-import platform.Foundation.NSSearchPathForDirectoriesInDomains
-import platform.Foundation.NSString
-import platform.Foundation.NSUTF8StringEncoding
-import platform.Foundation.NSUserDomainMask
-import platform.Foundation.writeToFile
-
-fun createFile(fileName: String, content: String): String {
-    val fileManager = NSFileManager.defaultManager
-    val paths = NSSearchPathForDirectoriesInDomains(
-        directory = NSDocumentDirectory,
-        domainMask = NSUserDomainMask,
-        expandTilde = true
-    )
-
-    // Obtém o diretório de documentos do usuário
-    val documentsDirectory = paths.firstOrNull() as? String
-        ?: return "Error: Unable to get documents directory"
-
-    // Caminho completo do arquivo
-    val filePath = "$documentsDirectory/$fileName"
-
-    // Escreve o conteúdo no arquivo
-    @OptIn(kotlinx.cinterop.ExperimentalForeignApi::class)
-    val success = (content as NSString).writeToFile(
-        filePath,
-        atomically = true,
-        encoding = NSUTF8StringEncoding,
-        error = null
-    )
-
-    return if (success) filePath else "Error: Unable to write file"
-}
+import org.koin.compose.KoinApplication
 
 fun MainViewController() = ComposeUIViewController {
-    App()
+    KoinApplication(
+        application = {
+            modules(
+                printerCommonDiModule()
+            )
+        }
+    ) {
+        App()
+    }
 }
