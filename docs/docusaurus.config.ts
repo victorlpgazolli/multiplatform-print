9e4164abfb62db42694597877aa3609e27311fa4
victorlpgazolli <victorlpgazolli@gmail.com>
2025-03-09 16:13:32 -0300

    fix: url

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
-  url: 'https://victorlpgazolli.github.io/multiplatform-print/',
+  url: 'https://multiplatform-print.victorlpgazolli.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
